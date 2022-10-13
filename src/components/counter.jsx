05bad7658945e7b23e750a8b74466568f6bfa4e3
medwards <maxedwards20@gmail.com>
2022-10-13 11:43:31 -0600

    Added counters component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,7 +16,7 @@ class Counter extends Component {
             <div>
                 <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
                 <button 
-                onClick={() => this.handleIncrement(product)} 
+                onClick={() => this.handleIncrement()} 
                 className="btn btn-secondary btn-sm">Increment</button>
             </div>
         );
